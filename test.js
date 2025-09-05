667b464675cc9c73292677d76272a5a8ed7ca04f
HamzaSheikh7743 <sheikhhamza7743@gmail.com>
2025-09-06 04:40:54 +0500

    comparison

--- test.js
+++ test.js
@@ -1,14 +1,32 @@
 
-let score = "33abc";
+//let score = "33abc";
+//
+//console.log(typeof score);
+//console.log(typeof(score));
+//
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//
+//let isLoggedIn = 1;
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
-console.log(typeof score);
-console.log(typeof(score));
+ 
+//console.log("2">1);
+//console.log("02">1);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+ //===
+ console.log("2"===2);
+ 
