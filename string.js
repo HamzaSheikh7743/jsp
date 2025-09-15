a9e05e9fdab0b00ee3ebc024f8ffb8806de2dc3f
HamzaSheikh7743 <sheikhhamza7743@gmail.com>
2025-09-16 01:22:35 +0500

    guessnumber

---
+++ string.js
@@ -0,0 +1,8 @@
+const name = "string.js";
+const repocount = 50;
+
+// console .log(name, repocount + " value");
+
+console.log(`Hello, my namre is ${name} and my repo count is ${repocount}`); 
+
+const ganeName = String("chess");
